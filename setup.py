f46a32fbcbe942602ead8b492ca03eac8b6aad1d
Marc Torrent <marccuairan@gmail.com>
2020-02-17 16:08:05 +0100

    Added function to calculate the single sided power spectral density

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from data_analysis_tools import __version__ as current_version
 # NOTES for updating this file:
 # 1) for version update in the data_analysis_tools.__init__
 # 2) update the following comment_on_changes
-comment_on_changes = 'Added general folder path for selecting data paths'
+comment_on_changes = 'changed version to test installation'
 
 setup(
     name='data_analysis_tools',
